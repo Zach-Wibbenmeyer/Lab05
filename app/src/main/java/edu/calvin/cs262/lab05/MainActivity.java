067779b501ce5b08f1fa44bdb2a814fc19f8bfd5
postmacaleb <postmacaleb@gmail.com>
2016-10-07 15:47:25 -0400

    Working Product

--- app/src/main/java/edu/calvin/cs262/lab05/MainActivity.java
+++ app/src/main/java/edu/calvin/cs262/lab05/MainActivity.java
@@ -1,18 +1,32 @@
 package edu.calvin.cs262.lab05;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        pref =  PreferenceManager.getDefaultSharedPreferences(this);
+        showPreferences();
+
+    }
+
+    private void showPreferences() {
+        TextView prefText = (TextView) findViewById(R.id.prefTextView);
+        prefText.setText("Preference: " + pref.getBoolean("preference", false));
     }
 
     @Override
@@ -25,10 +39,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_settings:
-                startActivity( new Intent(getApplicationContext(), SettingsActivity.class));
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        showPreferences();
     }
 }
---
+++ app/src/main/java/edu/calvin/cs262/lab05/SettingsFragment.java
@@ -0,0 +1,30 @@
+package edu.calvin.cs262.lab05;
+
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
